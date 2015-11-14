46f25f8d818aad9496cc53dcbb5a24d017c12cd8
earl <drolarumil@gmail.com>
2015-11-13 21:31:48 -0800

    I dont know

--- js/grayscale.js
+++ js/grayscale.js
@@ -15,6 +15,39 @@ $(window).scroll(function() {
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
+	$('#save-route').click(function() {
+	alert("FUCK");
+
+		$.post('/traffic/set_route', {
+			addr1_from: "14300 Newport Ave",
+			add2_from: "#81",
+			city_from: "Tustin",
+			state_from: "CA",
+			addr1_from: "1073 N Batavia St",
+			city_from: "Orange",
+			state_from: "CA",
+
+		}, function(data, textStatus) {
+			alert(textStatus);
+			if ('error' in data) {
+				alert("<strong>Error!</strong> " + data.error);
+				return;
+			}
+			alert(data.error);
+			if (textStatus != 'success') {
+				alert("<strong>Error!</strong> Unable to contact server", "err");
+				return;
+			}
+
+
+			alert('<strong>Success!</strong> Processing has begun. Check the <b>Results</b> tab.', "success");
+			upload();
+		}).fail(function() {
+			alert("<strong>Error!</strong> Unable to contact server", "err");
+		}).always(function() {
+			console.log("Done");
+		});
+	});
 	$('a.page-scroll').bind('click', function(event) {
 		var $anchor = $(this);
 		$('html, body').stop().animate({
@@ -25,160 +58,13 @@ $(function() {
 });
 
 $(function() {
-	$('#submit-route').bind('click', function(event) {
-		console.log("worked");
+	$('#save-route').bind('click', function(event) {
+
 	});
 });
+
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
 	$('.navbar-toggle:visible').click();
 });
-
-// Google Maps Scripts
-// When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
-
-function init() {
-	// Basic options for a simple Google Map
-	// For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-	var mapOptions = {
-		// How zoomed in you want the map to start at (always required)
-		zoom: 15,
-
-		// The latitude and longitude to center the map (always required)
-		center: new google.maps.LatLng(40.6700, -73.9400), // New York
-
-		// Disables the default Google Maps UI components
-		disableDefaultUI: true,
-		scrollwheel: false,
-		draggable: false,
-
-		// How you would like to style the map. 
-		// This is where you would paste any style found on Snazzy Maps.
-		styles: [{
-			"featureType": "water",
-			"elementType": "geometry",
-			"stylers": [{
-				"color": "#000000"
-			}, {
-				"lightness": 17
-			}]
-		}, {
-			"featureType": "landscape",
-			"elementType": "geometry",
-			"stylers": [{
-				"color": "#000000"
-			}, {
-				"lightness": 20
-			}]
-		}, {
-			"featureType": "road.highway",
-			"elementType": "geometry.fill",
-			"stylers": [{
-				"color": "#000000"
-			}, {
-				"lightness": 17
-			}]
-		}, {
-			"featureType": "road.highway",
-			"elementType": "geometry.stroke",
-			"stylers": [{
-				"color": "#000000"
-			}, {
-				"lightness": 29
-			}, {
-				"weight": 0.2
-			}]
-		}, {
-			"featureType": "road.arterial",
-			"elementType": "geometry",
-			"stylers": [{
-				"color": "#000000"
-			}, {
-				"lightness": 18
-			}]
-		}, {
-			"featureType": "road.local",
-			"elementType": "geometry",
-			"stylers": [{
-				"color": "#000000"
-			}, {
-				"lightness": 16
-			}]
-		}, {
-			"featureType": "poi",
-			"elementType": "geometry",
-			"stylers": [{
-				"color": "#000000"
-			}, {
-				"lightness": 21
-			}]
-		}, {
-			"elementType": "labels.text.stroke",
-			"stylers": [{
-				"visibility": "on"
-			}, {
-				"color": "#000000"
-			}, {
-				"lightness": 16
-			}]
-		}, {
-			"elementType": "labels.text.fill",
-			"stylers": [{
-				"saturation": 36
-			}, {
-				"color": "#000000"
-			}, {
-				"lightness": 40
-			}]
-		}, {
-			"elementType": "labels.icon",
-			"stylers": [{
-				"visibility": "off"
-			}]
-		}, {
-			"featureType": "transit",
-			"elementType": "geometry",
-			"stylers": [{
-				"color": "#000000"
-			}, {
-				"lightness": 19
-			}]
-		}, {
-			"featureType": "administrative",
-			"elementType": "geometry.fill",
-			"stylers": [{
-				"color": "#000000"
-			}, {
-				"lightness": 20
-			}]
-		}, {
-			"featureType": "administrative",
-			"elementType": "geometry.stroke",
-			"stylers": [{
-				"color": "#000000"
-			}, {
-				"lightness": 17
-			}, {
-				"weight": 1.2
-			}]
-		}]
-	};
-
-	// Get the HTML DOM element that will contain your map 
-	// We are using a div with id="map" seen below in the <body>
-	var mapElement = document.getElementById('map');
-
-	// Create the Google Map using out element and options defined above
-	var map = new google.maps.Map(mapElement, mapOptions);
-
-	// Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-	var image = 'img/map-marker.png';
-	var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
-	var beachMarker = new google.maps.Marker({
-		position: myLatLng,
-		map: map,
-		icon: image
-	});
-}
